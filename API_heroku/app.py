8c403548579a70fbd3020e990596f910d87e044e
Florian-BOUDON <97687795+Florian-BOUDON@users.noreply.github.com>
2023-06-20 11:49:45 +0200

    V1

--- API_heroku/app.py
+++ API_heroku/app.py
@@ -14,9 +14,21 @@ app = Flask(__name__)
 @app.route('/data', methods=['GET'])
 def get_data():
     param = request.args.get("proba")
-    param = int(param)
-    p=df_proba.loc[param].to_dict()
-    return jsonify(p)
+    
+    if param is None:
+        return jsonify({"error": "Missing parameter 'proba'"}), 400
+    
+    try:
+        param = int(param)
+    except ValueError:
+        return jsonify({"error": "Invalid parameter 'proba'"}), 400
+    
+    if param not in df_proba.index:
+        return jsonify({"error": "Invalid parameter value for 'proba'"}), 400
+    
+    p = df_proba.loc[param].to_dict()
+    return jsonify(p), 200
+
 
 
 # Définir l'endpoint POST
@@ -24,12 +36,25 @@ def get_data():
 def prediction():
     # Récupérer le dictionnaire JSON à partir de la requête
     data = request.get_json()
-    df = pd.DataFrame(data)
-    proba = pipeline.predict_proba(df)[0][0]
-    proba = float(proba)
+
+     # Vérifier si le JSON est présent et contient 30 clés
+    if data is None or len(data) != 30:
+        return jsonify({'error': 'Invalid JSON or incorrect number of keys'}), 400
+    
+    # Créer un DataFrame à partir du dictionnaire JSON
+    df = pd.DataFrame([data])
+
+    # Effectuer la prédiction en utilisant le pipeline
+    try:
+        proba = pipeline.predict_proba(df)[0][0]
+        proba = float(proba)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
     # Renvoyer la réponse sous forme de JSON
     response = {'probability': proba}
-    return jsonify(response)
+    return jsonify(response), 200
+
 
 # Lancer l'application Flask
 if __name__ == '__main__':
